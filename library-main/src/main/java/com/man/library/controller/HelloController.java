073aaa9bc51f77d8b599b1b78b3aa16c831fd78e
man <1848446960@qq.com>
2020-12-26 15:29:28 +0800

    测试

---
+++ library-main/src/main/java/com/man/library/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.man.library.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * desc:
+ * created by man
+ * created on 2020/12/26
+ */
+@RestController
+public class HelloController {
+}
