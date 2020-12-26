94137479bba8aaefbac46ad92ce20e6c47727484
man <1848446960@qq.com>
2020-12-26 15:23:56 +0800

    项目基础架构

---
+++ library-mail/src/test/java/com/man/librarymail/LibraryMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.man.librarymail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryMailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ library-search/src/test/java/com/man/librarysearch/LibrarySearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.man.librarysearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
